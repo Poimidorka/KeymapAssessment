d5407b2fde1444a50cf1f75b8ba25cb4c212a08e
poimidorka <kanafeevmark@gmail.com>
2024-05-04 00:04:17 +0200

    Added README.md and fixed Number.kt file

--- src/main/kotlin/util/Number.kt
+++ src/main/kotlin/util/Number.kt
@@ -9,23 +9,18 @@ import javax.lang.model.type.ErrorType
  * @property data the list that holds numbers for the char
  */
 class Number(private val data: ArrayList<Digit>) {
+    constructor(data: ArrayList<Char>) : this(arrayListOf<Digit>()) {
+        for (element in data) {
+           pushBack(element)
+        }
+    }
     /**
      * Adds a new digit to the number.
      * @param char the character representing the digit to add
      * @throws Error if the character is not a valid digit
      */
-
-    constructor() {
-
-    }
-
-    init {
-        for (element in data.iterator()) {
-        }
-    }
-
     public fun pushBack(char: Char) {
-        if (!(Digit.alphabet.contains(char))) {
+        if (!(Digit.checkCharacter(char))) {
             throw WrongCharacterError("Adding '$char' into a Number class that is not in the alphabet")
         }
         data.add(Digit(char))
@@ -38,7 +33,10 @@ class Number(private val data: ArrayList<Digit>) {
  */
 class Digit(private val char: Char) {
     companion object {
-        val alphabet: HashSet<Char> = hashSetOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+        private val alphabet: HashSet<Char> = hashSetOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+        public fun checkCharacter(char: Char) : Boolean {
+            return alphabet.contains(char)
+        }
     }
     fun toChar(): Char {
         return char
@@ -48,5 +46,4 @@ class Digit(private val char: Char) {
 /**
  * Represents the specific error for the wrong digit
  */
-class WrongCharacterError(val errorMessage: String) : Error() {
-}
+class WrongCharacterError(val errorMessage: String) : Error()
