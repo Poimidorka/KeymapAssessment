8bc605fc5601838d3d66ff8744e2bd88e56bfad8
poimidorka <kanafeevmark@gmail.com>
2024-05-04 19:21:12 +0200

    Updated Number.kt and Operation.kt

--- src/main/kotlin/util/Number.kt
+++ src/main/kotlin/util/Number.kt
@@ -5,19 +5,30 @@ package org.example.util
  * Implementation supports a variety length of digits (more than int)
  * @property data the list that holds numbers for the char
  */
-class Number(private val data: ArrayList<Digit>) {
-    constructor(data: ArrayList<Char>) : this(arrayListOf<Digit>()) {
+data class Number(private val data: ArrayList<Digit>) {
+    constructor(data: String) : this(arrayListOf<Digit>()) {
         for (element in data) {
            add(element)
         }
     }
+    constructor() : this(arrayListOf())
     /**
      * Adds a new digit to the number.
      * @param char the character representing the digit to add
      * @throws Error if the character is not a valid digit
      */
-    public fun add(char: Char) {
+    fun add(char: Char) {
         data.add(Digit(char))
+    }
+    companion object {
+        fun checkNumber(data: String): Boolean {
+            for (element in data) {
+                if (!Digit.checkCharacter(element)) {
+                    return false
+                }
+            }
+            return true
+        }
     }
 }
 
@@ -25,7 +36,7 @@ class Number(private val data: ArrayList<Digit>) {
  * Represents a single digit in a number.
  * @property char the character representing the digit
  */
-class Digit(private val char: Char) {
+data class Digit(private val char: Char) {
     init {
         if (!checkCharacter(char)) {
             throw WrongCharacterError("Adding '$char' into a Number class that is not in the alphabet")
@@ -33,6 +44,10 @@ class Digit(private val char: Char) {
     }
     companion object {
         private val alphabet: HashSet<Char> = hashSetOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+        /**
+         * Checks that the character is in the alphabet
+         * @param char the character to check
+         */
         public fun checkCharacter(char: Char) : Boolean {
             return alphabet.contains(char)
         }
